3649d6b63eaff11ad36cfc3ccecec4d0aecbf7aa
Daniel Polevikov <danielpol169@gmail.com>
2022-09-27 16:12:21 +0300

    Load Jobs page and enter keywords and locatioan features are added

--- main.py
+++ main.py
@@ -32,10 +32,36 @@ class EasyApplyLinkedin:
         time.sleep(1)
         login_pass.send_keys(Keys.RETURN)
 
+    def load_jobs_page(self):
+        """This function goes to the 'Jobs' section a looks for all the jobs that matches the keywords and location"""
+
+        # go to Jobs
+        jobs_link = self.driver.find_element(By.LINK_TEXT, 'Jobs')
+        jobs_link.click()
+
+    def enter_keywords_and_location(self):
+        # search based on keywords and location and hit enter
+        search_keywords = self.driver.find_element(By.XPATH, "//input[starts-with(@id,'jobs-search-box-keyword')]")
+        search_keywords.clear()
+        search_keywords.send_keys(self.keywords)
+        time.sleep(1)
+        search_location = self.driver.find_element(By.XPATH, "//input[starts-with(@id,'jobs-search-box-location')]")
+        search_location.clear()
+        search_location.send_keys(self.location)
+        time.sleep(1)
+        search_location.send_keys(Keys.RETURN)
+
 
 if __name__ == "__main__":
     with open('config.json') as config_file:
         data = json.load(config_file)
 
     bot = EasyApplyLinkedin(data)
+
     bot.login_linkedin()
+    time.sleep(3)
+
+    bot.load_jobs_page()
+    time.sleep(3)
+
+    bot.enter_keywords_and_location()
